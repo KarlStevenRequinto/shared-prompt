5d4a7ccd31b46e0e5cff72320374bac13767589a
KarlStevenRequinto <lerdrobott@gmail.com>
2025-03-20 00:15:30 +0800

    revised texts

--- app/page.jsx
+++ app/page.jsx
@@ -3,12 +3,13 @@ import Feed from "@components/Feed";
 const Home = () => (
     <section className="w-full flex-center flex-col">
         <h1 className="head_text text-center">
-            Discover & Share
+            Share, Explore, and Inspire
             <br className="max-md:hidden" />
-            <span className="orange_gradient text-center"> AI-Powered Prompts</span>
+            <span className="orange_gradient text-center">AI Prompts</span>
         </h1>
         <p className="desc text-center">
-            Prompt It is an open-source AI prompting tool for modern world to discover, create and share creative prompts
+            Prompt It is an open-source AI prompting tool designed to inspire creativity, helping you discover, craft, and share powerful prompts
+            effortlessly in the modern world! 🚀
         </p>
 
         <Feed />
